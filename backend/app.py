ebaf9ea91f1d5cfbbe39f3844085334770dfc71d
kholl28 <153400372+kholl28@users.noreply.github.com>
2025-05-16 15:56:39 -0400

    Fix: import os in app.py to resolve NameError

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,7 @@ User-inputted information will be piped into the final machine learning model in
 
 # IMPORTING REQUIRED PACKAGES
 # ==================
+import os
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from datetime import datetime, timedelta

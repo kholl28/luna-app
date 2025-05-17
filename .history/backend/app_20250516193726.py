0ab6d76097bc26750160fa7d9711705bac716c42
kholl28 <153400372+kholl28@users.noreply.github.com>
2025-05-16 20:13:30 -0400

    reverted app.py to original

--- .history/backend/app_20250516193726.py
+++ .history/backend/app_20250516193726.py
@@ -64,7 +64,7 @@ def predict():
         print("Received data:", data)
 
         # Check if all required fields are present
-        required_fields = ["thirdDate", "cycle_1", "cycle_2", "Age", "Feet", "Inches, Weight"]
+        required_fields = ["thirdDate", "cycle_1", "cycle_2", "Age", "Feet", "Inches, ]
         for field in required_fields:
             if field not in data:
                 return jsonify({"error": f"Missing required field: {field}"}), 400
